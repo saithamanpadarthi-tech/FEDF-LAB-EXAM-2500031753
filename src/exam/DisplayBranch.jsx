d397daff8b6f59c19fb826cd16aaafa8aa25a83d
Thaman <2500031753@kluniversity.in>
2026-03-13 12:28:18 +0530

    modified DisplayBranch

--- src/exam/DisplayBranch.jsx
+++ src/exam/DisplayBranch.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function DisplayBranch() {
+  const [branch,setBranches] = useState([]);
+
+  useEffect(() => {
+    const storedBranches = JSON.parse(localStorage.getItem('BranchAdded')) || [];
+    setBranches(storedBranches);
+  },[])
   return (
     <div>
-      Here you can view Added Branches
+     {branch.length === 0? (<h1>NO USERS</h1>)
+     :(<table border={1} cellPadding={10} cellSpacing={10}>
+      <tr>
+      <th>Name</th>
+      <th>Id</th>
+      <th>Email</th>
+      <th>year</th>
+      <th>Branch</th>
+      </tr>
+
+      {branch.map((branch,index) => (
+        <tr>
+          <td>{branch.name}</td>
+          <td>{branch.ID}</td>
+          <td>{branch.email}</td>
+          <td>{branch.year}</td>
+          <td>{branch.branch}</td>
+
+
+        </tr>
+      ))}
+     </table>)}
     </div>
   )
 }
